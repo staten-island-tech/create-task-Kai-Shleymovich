2b834d3d2cd5f45e31469ac266e4858e55dae141
Kai-Shleymovich <124058665+Kai-Shleymovich@users.noreply.github.com>
2024-01-17 13:20:11 -0500

    started

---
+++ vite-project/main.js
@@ -0,0 +1,12 @@
+import './style.css'
+const num1 = Number(prompt('enter your first number'))
+const num2 = Number(prompt('enter your second number'))
+
+const addition = (num1 + num2)
+console.log(addition)
+const subtraction = (num1 - num2)
+console.log(subtraction)
+const multiplication = (num1 * num2)
+console.log(multiplication)
+const division = (num1 / num2)
+console.log (division)
